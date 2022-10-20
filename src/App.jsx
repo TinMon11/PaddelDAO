c2baa054cac64c628976675e6a019d12bb404ac1
TinMon11 <tincho_mondino@hotmail.com>
2022-10-20 04:45:03 -0300

    Metamask connection. Frontend Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to PaddelDAO</h1>
+        <h2>The First DAO for Paddel Lovers & Inverstors</h2>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Welcome to the Club 🥎<h1>
+      </h1> Let's start.</h1>
     </div>
   );
-};
+}
+
+
 
 export default App;
