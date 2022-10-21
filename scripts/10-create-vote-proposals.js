abfff634802be28cff891df1e693bcdf1751c1ac
TinMon11 <tincho_mondino@hotmail.com>
2022-10-21 08:24:37 -0300

    Create Vote Proposal first step

---
+++ scripts/10-create-vote-proposals.js
@@ -0,0 +1,73 @@
+import sdk from "./1-initialize-sdk.js";
+import { ethers } from "ethers";
+
+// This is our governance contract.
+const vote = sdk.getVote("0x8eB7CF89de5435FbaFe9F91F4217440f59242eA0");
+
+// This is our ERC-20 contract.
+const token = sdk.getToken("0x8D1A7B42ab381500F46b9e8DB5B4A1202b5A6B24");
+
+(async () => {
+  try {
+    // Create proposal to mint 300,000 new token to the treasury.
+    const amount = 300_000;
+    const description = "Should PaddelDAO mint an additional " + amount + " tokens into the treasury?";
+    const executions = [
+      {
+        // Our token contract that actually executes the mint.
+        toAddress: token.getAddress(),
+        // Our nativeToken is ETH. nativeTokenValue is the amount of ETH we want
+        // to send in this proposal. In this case, we're sending 0 ETH.
+        // We're just minting new tokens to the treasury. So, set to 0.
+        nativeTokenValue: 0,
+        // We're doing a mint! And, we're minting to the vote, which is
+        // acting as our treasury.
+        // in this case, we need to use ethers.js to convert the amount
+        // to the correct format. This is because the amount it requires is in wei.
+        transactionData: token.encoder.encode(
+          "mintTo", [
+          vote.getAddress(),
+          ethers.utils.parseUnits(amount.toString(), 18),
+        ]
+        ),
+      }
+    ];
+
+    await vote.propose(description, executions);
+
+    console.log("✅ Successfully created proposal to mint tokens");
+  } catch (error) {
+    console.error("failed to create first proposal", error);
+    process.exit(1);
+  }
+
+  try {
+    // Create proposal to transfer ourselves 20,000 tokens for being awesome.
+    const amount = 20_000;
+    const description = "Should PaddelDAO transfer " + amount + " tokens from the treasury to " +
+      process.env.WALLET_ADDRESS + " for being awesome?";
+    const executions = [
+      {
+        // Again, we're sending ourselves 0 ETH. Just sending our own token.
+        nativeTokenValue: 0,
+        transactionData: token.encoder.encode(
+          // We're doing a transfer from the treasury to our wallet.
+          "transfer",
+          [
+            process.env.WALLET_ADDRESS,
+            ethers.utils.parseUnits(amount.toString(), 18),
+          ]
+        ),
+        toAddress: token.getAddress(),
+      },
+    ];
+
+    await vote.propose(description, executions);
+
+    console.log(
+      "✅ Successfully created proposal to reward ourselves from the treasury, let's hope people vote for it!"
+    );
+  } catch (error) {
+    console.error("failed to create second proposal", error);
+  }
+})();
